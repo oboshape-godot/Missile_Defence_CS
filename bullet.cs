a1d3b1cf8f1dab7ca8206ce6f806ff928bb44747
DarrenMcBain <dar_mcbain@yahoo.co.uk>
2023-11-08 17:23:18 +0000

    3_MD_GCS : C# Scripts & Bullet Movement

---
+++ bullet.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class bullet : Area2D
+{
+	public int speed = 400;
+	Vector2 velocity = new Vector2(0f,0f);
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Hello World!");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double _delta)
+	{
+		velocity = Vector2.Right.Rotated(Rotation) * speed * (float)_delta;
+		Translate(velocity);
+	}
+}
